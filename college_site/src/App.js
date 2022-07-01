85a4a20e943ddb3e6e9a8f15cdc04ed1e8a60cb9
DeepakSingh260 <alexmha260@gmail.com>
2022-07-01 23:53:03 +0530

    Fixed Nav Bar

--- college_site/src/App.js
+++ college_site/src/App.js
@@ -15,9 +15,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [isOpen, setIsOpen] = React.useState(false)
+  
   return (
+    
     <div className="App">
       
+              <Routes>
+                <Route path="/" element={<Home />}/>
+                  
+                  <Route path="/about_us" element={<About_us />} />
+                  <Route path="/department" element={<Departments />} />
+                  <Route path="*" element={<Academics />} />
+              
+              </Routes>
+           
       <header className="App-header">
         <div className='draw_box'>
         <img  className = "class_logo" src={Logo_Src} alt="Image Failed to Load"/>
@@ -25,24 +36,57 @@ function App() {
           Government College of Engineering and Technology
         </h1>
         </div>
-      
+        
         <Navbar color="light" light expand="md">
                 <NavbarBrand href="/">Brand</NavbarBrand>
                 <NavbarToggler onClick={() => { setIsOpen(!isOpen) }} />
                 <Collapse isOpen={isOpen} navbar>
                     <Nav className="mr-auto" navbar>
                         <NavItem>
-                            <NavLink href="#">Home</NavLink>
+                            <NavLink href="/">Home</NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink href = "./subchild/About_us">About Us</NavLink>
+                            <NavLink href="/about_us">About Us</NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink href="/subchild/Department">Department</NavLink>
+                            <NavLink href="/department">Department</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/">Academics</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/">Admissions</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/">Tenders</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/">TEQIP</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/">Download</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/">MOU's</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/">NIRF</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/">AICTE EOA</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/">Contact us</NavLink>
                         </NavItem>
                     </Nav>
+
                 </Collapse>
             </Navbar>
+            
+            
+            
+               
+
       </header>
     </div>
   );

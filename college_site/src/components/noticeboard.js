cd2f504c5f2c464229c7d5a7dc6a9fb5d35b4b55
ayush391 <stringakaak@gmail.com>
2022-07-19 20:16:46 +0530

    added noticeboard and changed website header bg color

---
+++ college_site/src/components/noticeboard.js
@@ -0,0 +1,29 @@
+
+const noticeboard = () =>{
+    return (
+        <div class="card tg-widget tg-widgetnoticeboard">
+									<div class="card-title tg-widgettitle">
+										<h3 class='text-center'>Notice Board</h3>  
+									</div>
+									<div class=" card-body tg-widgetcontent">
+                                    <div class="input-group mb-3">
+                                        <input type="text" class="form-control" placeholder="Search Notification" aria-label="Search_Notification" aria-describedby="basic-addon1"/>
+                                        </div>
+										<ul class='list-group list-group-flush'>
+											<li class='list-group-item'>
+												<a href="javascript:void(0);">Adipisicing elit sed dotas eiusmod tempor incididunt utae labore etiat dolore magna aliqua enim ad minim veniam.</a>
+											</li>
+											<li class='list-group-item'>
+												<a href="javascript:void(0);">Labore etiat dolore magna aliqua enim ad minim veniam.</a>
+											</li>
+											<li class='list-group-item'>
+												<a href="javascript:void(0);">Duis aute irure dolor in reprehenderit.</a>
+											</li>
+										</ul>
+									</div>
+								</div>
+    )
+}
+
+
+export default noticeboard;

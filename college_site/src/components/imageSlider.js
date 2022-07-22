a28295ce6c24e6e7fafa98838864bea66410fce5
ayush391 <stringakaak@gmail.com>
2022-07-22 13:27:02 +0530

    added images slider and minor ui improvements

---
+++ college_site/src/components/imageSlider.js
@@ -0,0 +1,34 @@
+import gcet_1 from '../Assets/gcet_1.jpg';
+import gcet_2 from '../Assets/gcet_2.jpg';
+import gcet_3 from '../Assets/gcet_3.jpg';
+import gcet_4 from '../Assets/gcet_4.jpg';
+
+const imageslider = () => {
+    return (
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="item active">
+                    <img class="d-block w-80" src={gcet_1} alt="First slide" />
+                </div>
+                <div class="item">
+                    <img class="d-block w-80" src={gcet_2} alt="Second slide" />
+                </div>
+                <div class="item">
+                    <img class="d-block w-80" src={gcet_3} alt="Third slide" />
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+
+        </div>
+    )
+
+}
+
+export default imageslider;

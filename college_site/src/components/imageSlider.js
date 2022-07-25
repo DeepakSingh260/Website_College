3bcfc14b0f60c26daabac393d61410c5e44c5e87
ayush391 <stringakaak@gmail.com>
2022-07-25 11:53:09 +0530

    modifed the imagesSlider component to handle arguments

--- college_site/src/components/imageSlider.js
+++ college_site/src/components/imageSlider.js
@@ -3,35 +3,45 @@ import gcet_2 from '../Assets/gcet_2.jpg';
 import gcet_3 from '../Assets/gcet_3.jpg';
 import gcet_4 from '../Assets/gcet_4.jpg';
 
-const imageslider = () => {
+const imageslider = (props) => {
     return (
         <div className='carousel_div'>
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="item active">
-                    <img class="d-block" src={gcet_1} alt="First slide" />
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                            <div class="item active">
+                                <img class="d-block" src={props.href[0]} alt="First slide" />
+                            </div>
+                    {
+                        props.href.slice(1).map(e =>
+                            <div class="item">
+                                <img class="d-block" src={e} alt="First slide" />
+                            </div>
+                        )
+                    }
+                    {/* <div class="item active">
+                        <img class="d-block" src={props.href} alt="First slide" />
+                    </div>
+                    <div class="item">
+                        <img class="d-block" src={props.href} alt="Second slide" />
+                    </div>
+                    <div class="item">
+                        <img class="d-block" src={props.href} alt="Third slide" />
+                    </div> */}
                 </div>
-                <div class="item">
-                    <img class="d-block" src={gcet_2} alt="Second slide" />
-                </div>
-                <div class="item">
-                    <img class="d-block" src={gcet_3} alt="Third slide" />
-                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
         </div>
     )
 

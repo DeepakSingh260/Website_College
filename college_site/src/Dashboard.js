c9b751a881fc24a85354d6b81d26d313388b99d1
DeepakSingh260 <alexmha260@gmail.com>
2022-07-26 22:03:40 +0530

    notification update

--- college_site/src/Dashboard.js
+++ college_site/src/Dashboard.js
@@ -2,20 +2,40 @@ import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import "./Dashboard.css";
-import { auth, db, logout } from "./firebase";
+import { auth, db, logout ,app} from "./firebase";
+import { getDatabase, onValue, push, ref, set } from "firebase/database";
 import { query, collection, getDocs, where } from "firebase/firestore";
+
+
+
+function update(event,text){
+  const database = getDatabase(app)
+   const reference = ref(database , "Notifications/")
+   const new_update = {
+    Title:text
+   }
+   push(reference,new_update)
+  
+  alert(text)
+}
 
 function Dashboard() {
   const [user, loading, error] = useAuthState(auth);
   const [name, setName] = useState("");
   const navigate = useNavigate();
 
+  let state = {
+    text:""
+  };
+  const handleInput = event => {
+    state.text = event.target.value
+  };
+
   const fetchUserName = async () => {
     try {
       const q = query(collection(db, "users"), where("uid", "==", user?.uid));
       const doc = await getDocs(q);
       const data = doc.docs[0].data();
-
       setName(data.name);
     } catch (err) {
       console.error(err);
@@ -31,6 +51,19 @@ function Dashboard() {
   }, [user, loading]);
 
   return (
+    <div >
+      <h1>Notification Update</h1>
+      <form className="update_form">
+        <label>
+          Update:
+          <input type="text" onChange={handleInput} name="Update" />
+        </label>
+       
+      </form>
+      <button onClick={(e) => {
+      update(e, state.text);
+   }}>Push</button>
+
     <div className="dashboard">
       <div className="dashboard__container">
         Logged in as
@@ -40,6 +73,7 @@ function Dashboard() {
           Logout
         </button>
       </div>
+    </div>
     </div>
   );
 }

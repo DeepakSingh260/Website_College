cfcf4a349b544bbd18532baf3b7ac909484ecd48
ayush391 <stringakaak@gmail.com>
2022-07-30 11:51:04 +0530

    update club cards

---
+++ college_site/src/subchild/club.js
@@ -0,0 +1,10 @@
+
+const club = (props) =>{
+    return (
+        <div className="container">
+                <img className="img-fluid" src={props.href}></img>
+        </div>
+    );
+}
+export default club;
+

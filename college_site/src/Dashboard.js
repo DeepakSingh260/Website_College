f5aa4bbcc8020711cc714be739add0dbae8b1e53
ayush391 <stringakaak@gmail.com>
2022-10-13 20:12:20 +0530

    added upload syllbus feature

--- college_site/src/Dashboard.js
+++ college_site/src/Dashboard.js
@@ -80,8 +80,41 @@ function Dashboard() {
     Img: null,
     ImgLink: ""
   }
+
+  let syllabusfile = {
+    branch: "",
+    year: "",
+    pdf: null,
+    pdfLink: ""
+  }
+
+
+  const [selectedBranch, setSelectedBranch] = useState('COMPUTER');
+  const [selectedYear, setSelectedYear] = useState('1');
+  const [selectedFile, setSelectedFile] = useState('');
+
+  const handleBranchChange = event => {
+    console.log('Label 👉️', event.target.selectedOptions[0].label);
+    console.log(event.target.value);
+    setSelectedBranch(event.target.value);
+  };
+  const handleYearChange = event => {
+    console.log('Label 👉️', event.target.selectedOptions[0].label);
+    console.log(event.target.value);
+    setSelectedYear(event.target.value);
+  };
+
+
+
+
   const handleInput = event => {
     state.title = event.target.value
+  };
+  const syllabusbranchInput = event => {
+    syllabusfile.branch = event.target.value
+  };
+  const syllabusyearInput = event => {
+    syllabusfile.year = event.target.value
   };
   const handleBody = event => {
     state.body = event.target.value
@@ -97,6 +130,10 @@ function Dashboard() {
   const handlePDF = event => {
     state.pdf = event.target.files[0]
     console.log(state.pdf.name)
+  }
+  const handleSyllabusPDF = event => {
+    setSelectedFile(event.target.files[0])
+    console.log(selectedFile.name)
   }
 
   const handleIMG = event => {
@@ -143,6 +180,18 @@ function Dashboard() {
         console.log("link gotten")
       })
       console.log('Uploaded the pdf file!');
+    });
+
+  }
+  const updateSyllabus = (e) => {
+    e.preventDefault()
+    const storageRef = refer(storage, 'syllabus_folder/' + selectedBranch + '/' + selectedBranch + "_" + selectedYear + ".pdf");
+    uploadBytes(storageRef, selectedFile).then((snapshot) => {
+      getDownloadURL(storageRef).then((val) => {
+        console.log("link gotten")
+        console.log(val)
+      })
+      console.log('Uploaded the syllabus pdf file!');
     });
 
   }
@@ -224,6 +273,53 @@ function Dashboard() {
         </button> */}
 
       </div>
+
+      <div className="create-notification-container">
+
+        <h2 className="heading">Upload Syllabus</h2>
+        <form className="update_form">
+          <div className="row">
+
+            <div className="col-lg-5 col-md-12">
+
+              {/* <input className="input_" type="text" placeholder="Name" onChange={handleName} name="Update" /> */}
+              {/* <textarea className="textarea_" cols='40' rows='50' type="text" placeholder="Enter Contribution" onChange={handleContribution} name="Update" /> */}
+              <select class='list_box' onChange={handleBranchChange} name="branch-names" id="branch-names">
+                <option className="list_box_option" value="COMPUTER">Computer Engineering</option>
+                <option className="list_box_option" value="Mechanical">Mechanical Engineering</option>
+                <option className="list_box_option" value="Civil">Civil Engineering</option>
+                <option className="list_box_option" value="Electrical">Electrical Engineering</option>
+                <option className="list_box_option" value="E_C">Electronics and Communication Engineering</option>
+              </select>
+              <select class='list_box' onChange={handleYearChange} name="semester-names" id="semester-names">
+                <option className="list_box_option" value="1">1st year</option>
+                <option className="list_box_option" value="2">2nd Year</option>
+                <option className="list_box_option" value="3">3rd Year</option>
+                <option className="list_box_option" value="4">4th Year</option>
+              </select>
+            </div>
+            <div className="col-lg-5 col-md-12">
+
+              <h2 className="sub_heading">Upload Syllabus PDF</h2  >
+              <input className="file_input" type="file" ref={fileInput} onChange={handleSyllabusPDF} />
+              <button onClick={(e) => { updateSyllabus(e); }} className='btn btn-danger' >
+                <span className='ms-2' >Upload</span>
+              </button>
+            </div>
+          </div>
+
+          {/* <div className="row">
+            <div className="col-lg-12 ">
+
+              <button onClick={(e) => {
+                console.log(contri)
+                postContri(e, contri.Name, contri.Contribution, contri.ImgLink);
+              }} className="btn btn-danger">Submit</button>
+            </div>
+          </div> */}
+
+        </form>
+      </div >
 
       <div className="create-notification-container">
 

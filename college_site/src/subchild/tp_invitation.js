71035b78726fef702339eb04c52a48ed0373a84a
ayush391 <stringakaak@gmail.com>
2022-10-17 09:26:59 +0530

    updated tp invitation page

--- college_site/src/subchild/tp_invitation.js
+++ college_site/src/subchild/tp_invitation.js
@@ -5,32 +5,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
-const Tp_invitation = () =>{
+const Tp_invitation = () => {
 
   return (
     <div className='About_us'>
       <div className="container Introduction">
- 
-        <h2>Invitation</h2>
-        <div>
-        <p>Dear Sir/Madam, </p>
-    <p>Government College of Engineering and Technology, Jammu is one of the prestigious Engineering College in India (established in 1994). The College is conducting five B.E. courses. The students are groomed to meet all the present and future requirements of the Industry.
-</p>
+        <div className="heading text-center">
 
-      
-      <p>We have B.E. courses in Computer Engg., Electronics and Communication Engg., Electrical Engg, Mechanical Engg. and Civil Engg. Our past students occupy the top-notch technical and managerial positions in various organizations in India. Some of these include HCL, TCS, Satyam Computers, INFOSYS, WIPRO, KPIT CUMMINS and many other leaders in the industry.</p>
-   
-   <p>Now our final year students are waiting with wings to fly over to the various prestigious companies like yours to show their worth and assist the company to fly sky high. You may either wish to pay us a visit, or call our students for interview at your location. Kindly intimate us also the date of your arrival so that we can make suitable arrangements for your stay. We shall eagerly wait for your response.</p>
-   <p>Thanking you in anticipation.</p>
-    <p>Training & Placement Officer<br/>
-         E-mail: gcet_jammu1994@rediffmail.com<br/>
-         Website: www.gcetjammu.org<br/>
-         Telefax: (91)191-2623087</p>
-
-	    </div>
+          <h1>Invitation</h1>
         </div>
-		</div>
-    
+        <div className="text_body text-center">
+          <p>Dear Sir/Madam, </p>
+          <p>Government College of Engineering and Technology, Jammu is one of the prestigious Engineering College in India (established in 1994). The College is conducting five B.E. courses. The students are groomed to meet all the present and future requirements of the Industry.
+          </p>
+
+
+          <p>We have B.E. courses in Computer Engg., Electronics and Communication Engg., Electrical Engg, Mechanical Engg. and Civil Engg. Our past students occupy the top-notch technical and managerial positions in various organizations in India. Some of these include HCL, TCS, Satyam Computers, INFOSYS, WIPRO, KPIT CUMMINS and many other leaders in the industry.</p>
+
+          <p>Now our final year students are waiting with wings to fly over to the various prestigious companies like yours to show their worth and assist the company to fly sky high. You may either wish to pay us a visit, or call our students for interview at your location. Kindly intimate us also the date of your arrival so that we can make suitable arrangements for your stay. We shall eagerly wait for your response.</p>
+          <p>Thanking you in anticipation.</p>
+          <b>
+            <p>Training & Placement Officer<br />
+              E-mail: gcet_jammu1994@rediffmail.com<br />
+              Website: www.gcetjammu.org<br />
+              Telefax: (91)191-2623087</p>
+          </b>
+
+        </div>
+      </div>
+    </div>
+
   );
 }
 export default Tp_invitation;
